112a7320099b7f78c926173c0527fcbca04380a8
James Coulson <github@coulson.ie>
2022-07-17 06:03:34 +0100

    initial commit

---
+++ Makefile
@@ -0,0 +1,13 @@
+.DEFAULT_GOAL := crypt
+.PHONY: all crypt clean
+
+all: clean crypt
+
+crypt:
+	@echo Generating iso file
+	sudo mkarchiso -v -w /tmp/archiso-tmp crypt/
+
+clean:
+	@echo Removing temp files
+	sudo rm -rf /tmp/archiso-tmp/
+	sudo rm -rf out/*
---
+++ crypt/profiledef.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="archlinux-crypt"
+iso_label="ARCH_$(date +%Y%m)"
+iso_publisher="James Coulson <arch@coulson.ie>"
+iso_application="GPG and SSL creation and management tools"
+iso_version="$(date +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-x64.grub.esp' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="erofs"
+airootfs_image_tool_options=('-zlz4hc,12' -E ztailpacking)
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+)
